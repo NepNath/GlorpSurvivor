0ea65a3235e409eb0ae0bddba5585efacae9580c
NepNath <neptonath@gmail.com>
2025-11-14 11:58:49 +0100

    handeled window creation & closing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <SFML/Graphics.hpp>
+#include <print>
+
+
 
 int main()
 {
 
-    sf::Window window(sf::VideoMode(800, 600), "SFML Window");
+    int windowWidth = 800, windowHeight = 600;
+
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML Window", sf::Style::Default);
+
+    while (window.isOpen())
+    {
+        // gestion d'événements
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Demande de fermeture (croix de fermeture, alt+f4, etc.)
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+                std::println("window has been closed");
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            {
+                window.close();
+                std::println("Escape key pressed, closing window");
+            }
+            //d'autre évènements peuvent être gérés ici
+        }
+
+        // Effacement de l'ancienne frame (framebuffer)
+        window.clear(sf::Color::Black);
+        // ... Ici on dessine les objets, sprites, textes, formes, etc. ...
+
+
+        window.display(); // Affichage de la nouvelle frame
+    }
 
     return 0;
 }
