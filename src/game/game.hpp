f3a4732cb746683a38c472aea793df895a318d47
NepNath <neptonath@gmail.com>
2026-01-06 10:34:27 +0100

    THIS COMMIT SHOULD BUILD ON CLONE

--- src/game/game.hpp
+++ src/game/game.hpp
@@ -5,37 +5,53 @@
 
 namespace game::main
 {
-    struct GAME_EXPORT Vector2d
+struct GAME_EXPORT Vector2d
+{
+    float x;
+    float y;
+    Vector2d()
+        : x(0.0f)
+        , y(0.0f)
     {
-        float x;
-        float y;
+    }
 
-        Vector2d() : x(0.0f), y(0.0f) {}
-        Vector2d(float x, float y) : x(x), y(y) {}
-       
-    };
-
-    struct GAME_EXPORT Movement
+    Vector2d(float x, float y)
+        : x(x)
+        , y(y)
     {
-        Vector2d direction;
-        float speed;
+    }
+};
 
-        Movement() : direction(0,0), speed(0) {}
-        Movement(Vector2d dir, float spd) : direction(dir), speed(spd) {}
-    };
-
-    struct GAME_EXPORT Projectile
+struct GAME_EXPORT Mouvement
+{
+    Vector2d direction;
+    float speed;
+    Mouvement()
+        : direction(0, 0)
+        , speed(0)
     {
-        Vector2d position;
-        sf::CircleShape shape;
-        Projectile(Vector2d pos, float size) : position(pos), shape(size)
-        {
-            shape.setPosition(pos.x, pos.y);
-            shape.setFillColor(sf::Color::Red);
-        }
-    };
+    }
+    Mouvement(Vector2d dir, float spd)
+        : direction(dir)
+        , speed(spd)
+    {
+    }
+};
 
-    GAME_EXPORT int GenerateRandomInt(int min, int max);
+struct GAME_EXPORT Projectile
+{
+    Vector2d position;
+    sf::CircleShape shape;
+    Projectile(Vector2d pos, float size)
+        : position(pos)
+        , shape(size)
+    {
+        shape.setPosition(pos.x, pos.y);
+        shape.setFillColor(sf::Color::Red);
+    }
+};
 
-    GAME_EXPORT int run();
-}
+GAME_EXPORT int GenerateRandomInt(int min, int max);
+
+GAME_EXPORT int run();
+} // namespace game::main
